c9bdb8315e96047cb4f7bbe11e388b9686b7f982
Pankaj Puri <pankajpuri016@gmail.com>
2023-07-17 03:30:21 +1200

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import Header from "./components/Header"
-import Home from "./components/Home"
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
       <Home />
     </div>
   );
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,88 +1,85 @@
-import React from 'react'
-import styled from "styled-components"
-import HOme from "./Home"
-import Fade from 'react-reveal/Fade'
+import React from "react";
+import styled from "styled-components";
 
-function Section({title, description, leftBtnText, rightBtnText, backgroundImg}) {
-    
+import Fade from "react-reveal/Fade";
+
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
   return (
-    <Wrap bgImage = {backgroundImg}>
-        <Fade bottom>
+    <Wrap bgImage={backgroundImg}>
+      <Fade bottom>
         <ItemText>
-            <h1>{title}</h1>
-            <p>{description}</p>
+          <h1>{title}</h1>
+          <p>{description}</p>
         </ItemText>
         <Button>
-            <ButtonGroup>
-                <LeftButton>{leftBtnText}</LeftButton>
-                {rightBtnText && 
-                    <RightButton>{rightBtnText}</RightButton>
-                }
-                
-            </ButtonGroup>
-            <DownArrow src="/images/down-arrow.svg"/>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGroup>
+          <DownArrow src="/images/down-arrow.svg" />
         </Button>
-        </Fade>
+      </Fade>
     </Wrap>
-  )
+  );
 }
 
-export default Section
+export default Section;
 
 const Wrap = styled.div`
-    width: 100vw;
-    height: 100vh;
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-image: ${props => `url("/images/${props.bgImage}")`};
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between; // vertical
-    align-items: center; // horizontal
-    `
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; // vertical
+  align-items: center; // horizontal
+`;
 const ItemText = styled.div`
-    padding-top: 15vh;
-    text-align:center;
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
-`
-const Button = styled.div`
- 
-`
+  padding-top: 15vh;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+const Button = styled.div``;
 const ButtonGroup = styled.div`
-    display: flex;
-    @media (max-width: 768px){
-        flex-direction: column; 
-    }
-    
-`
+  display: flex;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
 const LeftButton = styled.div`
-    Background-color: rgba(23,26,32,0.8);
-    height: 40px;
-    width: 256px;
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 100px;
-    opacity: 0.85;
-    text-transform: uppercase;
-    font-size: 12px;
-    cursor:pointer;
-    margin: 8px;
-`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
 const RightButton = styled(LeftButton)`
-Background:white;
-opacity: 0.65;
-color:rgba(23,26,32,0.8);
-
-`
+  background: white;
+  opacity: 0.65;
+  color: rgba(23, 26, 32, 0.8);
+`;
 
 const DownArrow = styled.img`
-height: 40px;
-overflow-x: hidden;
-animation: animateDown infinite 1.5s;
-
-`
+  height: 40px;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
+`;
